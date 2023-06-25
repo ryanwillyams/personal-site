e8fd0c72db05c72aad7be33c7c2d263ebb7028d3
Ryan Williams <ryanwillyamsdo@gmail.com>
2023-06-25 14:05:57 -0700

    3

--- src/data/projects.js
+++ src/data/projects.js
@@ -26,6 +26,15 @@ const data = [
     desc:
       'TwitterLite is a simple RESTful back-end implementation of a twitter-like social network. It gives clients the ability to create and access accounts, follow other accounts, and view timelines consisting of posts that are public, their own, or from people they follow. The application is built on the Python Bottle framework alongside a SQLite database. Our back-end is divided into two microservices',
   },
+  {
+    title: 'BreadBoard Clock',
+    subtitle: '8 bit assembly language clock',
+    link: 'https://github.com/ryanwillyams/BreadBoard-Clock-in-Assembly',
+    image: '/images/projects/My project4.png',
+    date: '2022-02-06',
+    desc:
+      'I dedicated substantial effort to creating an impressive project—an 8-bit breadboard clock connected to a Raspberry Pi 3. With meticulous attention to detail, I programmed the clock in ARM assembly language, composing over 1000 lines of code. The project involved illuminating lights in an 8-bit format to display the current PST time. It required a combination of hardware and software skills, including proficiency in breadboarding, circuit design, and interfacing with the Raspberry Pi. This undertaking demanded problem-solving abilities, precise execution, and a strong understanding of low-level programming. The project showcases my dedication, technical expertise, and ability to tackle complex challenges in electronics and programming.',
+  },
 
 ];
 

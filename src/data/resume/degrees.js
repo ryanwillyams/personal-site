91e6f87124c13abea36f7d25b96a25f85a757183
Ryan williams <70562805+ryanwillyams@users.noreply.github.com>
2023-08-15 16:10:59 -0700

    Update degrees.js

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -3,7 +3,7 @@ const degrees = [
     school: 'California State University, Fullerton',
     degree: 'B.S. Computer Science',
     link: 'https://drive.google.com/file/d/1Dah_gNYjUutmQQ93_eyJGMnpGyBmOegm/view?usp=sharing',
-    year: 2022,
+    year: 2023,
   },
 ];
 
